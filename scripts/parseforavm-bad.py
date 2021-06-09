bb96ed06c3a07537e1d1996d566147df941fe348
derpferpmerp <47375701+derpferpmerp@users.noreply.github.com>
2021-06-08 21:51:53 -0500

    Create parseforavm-bad.py

---
+++ scripts/parseforavm-bad.py
@@ -0,0 +1,12 @@
+import subprocess
+
+subprocess.call("mkdir Compatible;mkdir Incompatible", shell=True)
+for f in __import__("os").listdir(__import__("sys").argv[1]):
+    if f == "Compatible" or f == "Incompatible" or "&" in f:
+        continue
+    if "ass" in str(subprocess.check_output("swfdump -t %s" % f, shell=True)):
+        subprocess.call(f"rm -rf ./Compatible/{f};cp {f} ./Compatible", shell=True)
+        print(f"{f}: Compatible")
+    elif "as3" in str(subprocess.check_output("swfdump -t %s" % f, shell=True)):
+        subprocess.call(f"rm -rf ./Incompatible/{f};cp {f} ./Incompatible", shell=True)
+        print(f"{f}: Incompatible")
