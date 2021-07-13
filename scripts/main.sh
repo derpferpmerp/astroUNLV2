4e9cd98fb9315b46e0bfbe570730ce5339b872b3
derpferpmerp <cmfleming@eths202.org>
2021-07-13 16:54:26 -0500

    Added "Switch to Computer Mode" to HTML Files

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,9 +1,8 @@
 cd ../
 for file in *.swf; do
-    if [ -f "$file" ]; then
+	if [ -f "$file" ]; then
 	rm -rf "$file.html"
-        python3 ./scripts/mainexec.py "$file" > "$file.html"
-	echo "<a href=\"${file}.html\" style='color:blue;'>${file}</a><br>" >> "ALL-MAIN.html"
+	python3 ./scripts/mainexec.py "$file" > "$file.html"
 	echo "Completed File $file"
-    fi
+	fi
 done
